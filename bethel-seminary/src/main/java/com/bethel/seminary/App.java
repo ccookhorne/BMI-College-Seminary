7d7e9891f1c37de1031ff9f60c5c904c77a5be56
Joshua Cookhorne <jncookhorne@gmail.com>
2024-10-09 19:29:34 -0400

    starting backend for apis and whatnot

---
+++ bethel-seminary/src/main/java/com/bethel/seminary/App.java
@@ -0,0 +1,13 @@
+package com.bethel.seminary;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
