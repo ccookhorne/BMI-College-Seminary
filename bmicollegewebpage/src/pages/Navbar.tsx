70adc80853af057d541b591d26c07e5596c3c610
Joshua Cookhorne <jncookhorne@gmail.com>
2024-10-11 11:34:13 -0400

    started some changes for navbar

--- bmicollegewebpage/src/pages/Navbar.tsx
+++ bmicollegewebpage/src/pages/Navbar.tsx
@@ -8,35 +8,30 @@ const Navbar = () =>{
     const navigate = useNavigate();
 
 
-
-    const navigationCheck= (url: string) =>{
-      if(url === "/home"){
-        navigate(url)
-      }
-      else if ( url === "/student-life"){
-        navigate(url)
-      }
+    const navbarOpen = () =>{
+      // we will look at this later but it is a method that should call two different looks for the navbar
+      // one closed and one open, the open needs new css too open from the left 
+    
     }
-
-
-
 
 
     return (
         <>
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <a className="navbar-brand" href="#">Navbar</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  
+  
+  <button className="navbar-toggler" onClick={navbarOpen()}>
     <span className="navbar-toggler-icon"></span>
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <button className="nav-lin" onClick={() =>navigationCheck("/home")}>Home <span className="sr-only">(current)</span></button>
+        <button className="nav-lin" onClick={() =>navigate("/home")}>Home <span className="sr-only">(current)</span></button>
       </li>
       <li className="nav-item">
-      <button className="nav-lin" onClick={() => navigationCheck("/student-life")}>Home <span className="sr-only">(current)</span></button>
+      <button className="nav-lin" onClick={() => navigate("/student-life")}>Student Life <span className="sr-only">(current)</span></button>
       </li>
     </ul>
   </div>
